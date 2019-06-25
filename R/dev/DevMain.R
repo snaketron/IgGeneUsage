539c626766e006887247e167ad8e5c37d325e0e2
simo <simo.kitanovski@uni-due.de>
2019-06-25 13:07:50 +0200

    new dataset added

--- R/dev/DevMain.R
+++ R/dev/DevMain.R
@@ -11,16 +11,23 @@ input.J <- input[which(regexpr(pattern = "IGHJ", text = input$gene_name) != -1),
 rm(input)
 
 
-comparison.usage <- compareUsage(usage.data = input.J,
-                                 mcmc.warmup = 500,
-                                 mcmc.steps = 1500,
-                                 mcmc.chains = 2,
-                                 mcmc.cores = 2,
-                                 hdi.level = 0.95,
-                                 adapt.delta = 0.95,
-                                 max.treedepth = 13)
+usage.J <- compareUsage(usage.data = input.J,
+                        mcmc.warmup = 500,
+                        mcmc.steps = 1500,
+                        mcmc.chains = 2,
+                        mcmc.cores = 2,
+                        hdi.level = 0.95,
+                        adapt.delta = 0.95,
+                        max.treedepth = 13)
 
-comparison.usage$glm.summary
+usage.V <- compareUsage(usage.data = input.V,
+                        mcmc.warmup = 500,
+                        mcmc.steps = 1500,
+                        mcmc.chains = 2,
+                        mcmc.cores = 2,
+                        hdi.level = 0.95,
+                        adapt.delta = 0.95,
+                        max.treedepth = 13)
 
 
 
@@ -31,12 +38,12 @@ comparison.usage$glm.summary
 
 
 ggplot()+
-  geom_errorbar(data = comparison.usage$group.ppc$group.ppc,
+  geom_errorbar(data = usage.J$group.ppc$group.ppc,
                 aes(x = gene_name, ymin = ppc.L,
                     ymax = ppc.H, col = condition),
                 position = position_dodge(width = .8),
                 width = 0.75)+
-  geom_point(data = comparison.usage$group.ppc$individual.pct.data,
+  geom_point(data = usage.J$group.ppc$individual.pct.data,
              aes(x = gene_name, y = gene_usage_pct,
                  col = condition), shape = 21, size = 0.5,
              position = position_dodge(width = .8), fill = "black")+
@@ -45,10 +52,21 @@ ggplot()+
         legend.position = "top")
 
 
-comparison.usage$usage.data$
-t.test()
 
+ggplot()+
+  geom_errorbar(data = usage.V$group.ppc$group.ppc,
+                aes(x = gene_name, ymin = ppc.L,
+                    ymax = ppc.H, col = condition),
+                position = position_dodge(width = .8),
+                width = 0.75)+
+  geom_point(data = usage.V$group.ppc$individual.pct.data,
+             aes(x = gene_name, y = gene_usage_pct,
+                 col = condition), shape = 21, size = 0.5,
+             position = position_dodge(width = .8), fill = "black")+
+  theme_bw(base_size = 9)+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.4),
+        legend.position = "top")
 
-
-comparison.usage$glm.summary
+usage.V$t.test.stats
+usage.V$man.u.stats
 
