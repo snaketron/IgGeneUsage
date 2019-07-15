3d56e22d2f17972105bcbcd39b0aa677ca5ef1e1
Simo <simo.kitanovski@uni-due.de>
2019-07-16 00:29:30 +0200

    beta regression done, interesting

--- dev/DevProportions.R
+++ dev/DevProportions.R
@@ -17,12 +17,13 @@ Yp <- data.list$Y
 for(i in 1:nrow(Yp)) {
   Yp[i, ] <- Yp[i, ]/as.numeric(data.list$N)
 }
-Yp[which(Yp == 0, arr.ind = T), ] <- 10^(-16) # numerical stability log(0)
+Yp[which(Yp == 0, arr.ind = T)] <- 10^(-16) # numerical stability log(0)
 data.list$Yp <- Yp
 rm(Yp, i)
 
 
 model.proportions <- rstan::stan_model(file = "src/stan_files/case_by_case/proportion_multigene.stan")
+model.h.proportions <- rstan::stan_model(file = "src/stan_files/case_by_case/proportion_h_multigene.stan")
 
 
 
@@ -30,37 +31,37 @@ glm.prop <- rstan::sampling(object = model.proportions,
                             data = data.list,
                             chains = 4,
                             cores = 4,
-                            iter = 1000,
-                            warmup = 500,
-                            control = list(adapt_delta = 0.90,
+                            iter = 3000,
+                            warmup = 1000,
+                            control = list(adapt_delta = 0.95,
                                            max_treedepth = 10))
 
 
-summary(glm.prop, par = "beta_gene")$summary
+glm.h.prop <- rstan::sampling(object = model.h.proportions,
+                            data = data.list,
+                            chains = 4,
+                            cores = 4,
+                            iter = 3000,
+                            warmup = 1000,
+                            control = list(adapt_delta = 0.95,
+                                           max_treedepth = 10))
 
 
+s <- data.frame(summary(glm.h.prop, par = "beta_gene")$summary)
+s$gene_name <- data.list$gene_names
 
 
-e <- rstan::extract(object = glm)
+Mzibb <- get(load(file = "dev/ighv_hcv_zibb_model.RData"))
+z <- Mzibb$glm.summary
+z$gene_name == s$gene_name
 
-par(mfrow = c(2, 2))
-hist(e$Yhat_group[, 1]*100)
-hist(e$Yhat_group[, 2]*100)
+z$p.mean <- s$mean
+z$p.median <- s$X50.
+e <- rstan::extract(object = glm.h.prop)
+z$p.pmax <- getPmax(glm.ext = e)
+z$delta <- z$pmax - z$p.pmax
 
-
-plot(data.list$Y/data.list$N*100, col = as.factor(data.list$X))
-hist(e$Yhat_individual[, 15])
-
-hist(e$b_sample, breaks = 100)
-hist(e$Yhat_individual[, 5], breaks = 100)
-
-sum(e$b_sample < 0)/length(e$b_sample)
+plot(abs(z$effect_mean), abs(z$p))
 
 
-plot(density(100*1/(1 + exp(-(e$a_sample + e$b_sample*1)))), xlim = c(0, 0.5))
-points(density(100*1/(1 + exp(-(e$a_sample + e$b_sample*(-1))))), xlim = c(0, 0.5), type = "l")
-plot(d$gene_usage_count, col = as.factor(d$condition))
-data.list$N
-
-
-getHdi(vec = e$Yhat[, u], hdi.level = 0.95)
+plot(abs(z$pmax), abs(z$p.pmax))
--- src/stan_files/case_by_case/proportion_h_multigene.stan
+++ src/stan_files/case_by_case/proportion_h_multigene.stan
@@ -6,20 +6,32 @@ data {
 }
 
 parameters {
+  real alpha_grand;
+  real beta_grand;
+
+  real <lower = 0> alpha_sigma;
+  real <lower = 0> beta_sigma;
+  real <lower = 0> beta_gene_sigma;
+
+  vector [N_gene] alpha_raw;
+  vector [N_gene] beta_gene_raw;
+  vector [N_gene] beta_raw [N_sample];
+
   real <lower = 0> phi;
   real<lower = 0> tau;
-  vector [N_gene] alpha_gene;
-  vector [N_gene] beta_gene;
-  vector <lower = 0, upper = 1> [N_gene] beta_raw [N_sample];
-  real <lower = 0> beta_sigma;
 }
 
 
 transformed parameters {
+  vector [N_gene] alpha_gene;
+  vector [N_gene] beta_gene;
   vector <lower = 0> [N_gene] a [N_sample];
   vector <lower = 0> [N_gene] b [N_sample];
   vector [N_gene] beta_sample [N_sample];
 
+  // non-centered params
+  alpha_gene = alpha_grand + alpha_sigma * alpha_raw;
+  beta_gene = beta_grand + beta_gene_sigma * beta_gene_raw;
   for(i in 1:N_sample) {
     beta_sample[i] = beta_gene + beta_sigma * beta_raw[i];
     a[i] = inv_logit(alpha_gene + beta_sample[i]*X[i]) * phi;
@@ -33,13 +45,16 @@ model {
     Yp[, i] ~ beta(a[i], b[i]);
   }
 
-  alpha_gene ~ normal(0, 10);
-  beta_gene ~ normal(0, 5);
+  alpha_grand ~ normal(0, 10);
+  beta_grand ~ normal(0, 5);
 
   beta_sigma ~ cauchy(0, 1);
   for(i in 1:N_sample) {
     beta_raw[i] ~ normal(0, 1);
   }
+  alpha_raw ~ normal(0, 1);
+  beta_gene_raw ~ normal(0, 1);
+
 
   phi ~ exponential(tau);
   tau ~ gamma(3, 0.1);
