134236d4207e23a0e6b60042d585172d1c4867a4
Simo <simo.kitanovski@uni-due.de>
2019-07-26 18:54:19 +0200

    man-u testthat

--- tests/testthat/test-Frequentist.R
+++ tests/testthat/test-Frequentist.R
@@ -1,4 +1,4 @@
-context("Tests input rules")
+context("Tests Frequentist methods")
 
 
 test_that("T-test check", {
@@ -46,6 +46,52 @@ test_that("T-test check", {
   usage.data <- list(Y = matrix(data = numeric(length = 4), nrow = 1, ncol = 4),
                      X = c("a", "a", "b", "b"), N = c(1, 1, 1, 1), N_gene = 1)
   out <- getTTestStats(usage.data = usage.data)
+  expect_equal(object = out, expected = dummy.out)
+})
+
+
+
+
+
+
+test_that("U-test check", {
+
+
+  # CS: 0
+  dummy.out <- data.frame(u.test.pvalue = NA,
+                          u.test.wvalue = NA,
+                          u.test.fdr.pvalue = as.numeric(x = NA),
+                          u.test.bonf.pvalue = as.numeric(x = NA),
+                          stringsAsFactors = FALSE)
+  usage.data <- list(Y = matrix(data = numeric(length = 1), nrow = 1, ncol = 1),
+                     X = c("a"), N = c(1), N_gene = 1)
+  out <- getManUStats(usage.data = usage.data)
+  expect_equal(object = out, expected = dummy.out)
+
+
+  # CS: 1
+  dummy.out <- data.frame(u.test.pvalue = NaN,
+                          u.test.wvalue = 0.5,
+                          u.test.fdr.pvalue = as.numeric(x = NaN),
+                          u.test.bonf.pvalue = as.numeric(x = NaN),
+                          stringsAsFactors = FALSE)
+  rownames(dummy.out) <- "W"
+  usage.data <- list(Y = matrix(data = numeric(length = 2), nrow = 1, ncol = 2),
+                     X = c("a", "b"), N = c(1, 1), N_gene = 1)
+  out <- getManUStats(usage.data = usage.data)
+  expect_equal(object = out, expected = dummy.out)
+
+
+  # CS: 2
+  dummy.out <- data.frame(u.test.pvalue = NaN,
+                          u.test.wvalue = 1,
+                          u.test.fdr.pvalue = as.numeric(x = NaN),
+                          u.test.bonf.pvalue = as.numeric(x = NaN),
+                          stringsAsFactors = FALSE)
+  rownames(dummy.out) <- "W"
+  usage.data <- list(Y = matrix(data = numeric(length = 3), nrow = 1, ncol = 3),
+                     X = c("a", "b", "b"), N = c(1, 1, 1), N_gene = 1)
+  out <- getManUStats(usage.data = usage.data)
   expect_equal(object = out, expected = dummy.out)
 })
 
