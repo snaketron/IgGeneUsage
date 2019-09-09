f64bfec33392b9ce1b98e038def95b9049af4f85
Simo <simo.kitanovski@uni-due.de>
2019-09-09 20:09:29 +0200

    P-value -> NaN -> FDR -> NaN

--- tests/testthat/test-Frequentist.R
+++ tests/testthat/test-Frequentist.R
@@ -3,12 +3,11 @@ context("Tests Frequentist methods")
 
 test_that("T-test check", {
 
-  dummy.out <- data.frame(t.test.pvalue = 1,
+  dummy.out <- data.frame(t.test.pvalue = NA,
                           t.test.tvalue = NA,
                           t.test.L95 = NA,
                           t.test.H95 = NA,
-                          t.test.fdr.pvalue = 1,
-                          t.test.bonf.pvalue = 1,
+                          t.test.fdr.pvalue = as.numeric(NA),
                           stringsAsFactors = FALSE)
 
   # CS: 0
@@ -32,12 +31,11 @@ test_that("T-test check", {
   expect_equal(object = out, expected = dummy.out)
 
 
-  dummy.out <- data.frame(t.test.pvalue = 1,
+  dummy.out <- data.frame(t.test.pvalue = NaN,
                           t.test.tvalue = NaN,
                           t.test.L95 = NaN,
                           t.test.H95 = NaN,
-                          t.test.fdr.pvalue = 1,
-                          t.test.bonf.pvalue = 1,
+                          t.test.fdr.pvalue = NaN,
                           stringsAsFactors = FALSE)
   rownames(dummy.out) <- "t"
 
@@ -58,10 +56,9 @@ test_that("U-test check", {
 
 
   # CS: 0
-  dummy.out <- data.frame(u.test.pvalue = 1,
+  dummy.out <- data.frame(u.test.pvalue = NA,
                           u.test.wvalue = NA,
-                          u.test.fdr.pvalue = 1,
-                          u.test.bonf.pvalue = 1,
+                          u.test.fdr.pvalue = as.numeric(NA),
                           stringsAsFactors = FALSE)
   usage.data <- list(Y = matrix(data = numeric(length = 1), nrow = 1, ncol = 1),
                      X = c("a"), N = c(1), N_gene = 1)
@@ -70,10 +67,9 @@ test_that("U-test check", {
 
 
   # CS: 1
-  dummy.out <- data.frame(u.test.pvalue = 1,
+  dummy.out <- data.frame(u.test.pvalue = NaN,
                           u.test.wvalue = 0.5,
-                          u.test.fdr.pvalue = 1,
-                          u.test.bonf.pvalue = 1,
+                          u.test.fdr.pvalue = NaN,
                           stringsAsFactors = FALSE)
   rownames(dummy.out) <- "W"
   usage.data <- list(Y = matrix(data = numeric(length = 2), nrow = 1, ncol = 2),
@@ -83,10 +79,9 @@ test_that("U-test check", {
 
 
   # CS: 2
-  dummy.out <- data.frame(u.test.pvalue = 1,
+  dummy.out <- data.frame(u.test.pvalue = NaN,
                           u.test.wvalue = 1,
-                          u.test.fdr.pvalue = 1,
-                          u.test.bonf.pvalue = 1,
+                          u.test.fdr.pvalue = NaN,
                           stringsAsFactors = FALSE)
   rownames(dummy.out) <- "W"
   usage.data <- list(Y = matrix(data = numeric(length = 3), nrow = 1, ncol = 3),
