dc215dde3f4d99906258917332f28cca43f9d1f4
sktron <simo.kitanovski@uni-due.de>
2021-10-08 23:07:02 +0200

    Bugfix, tests

--- tests/testthat/test-model.R
+++ tests/testthat/test-model.R
@@ -9,18 +9,16 @@ test_that("stan model availability check", {
   
   expect_equal(object = model@model_name, expected = "zibb")
   expect_is(object = model, class = "stanmodel")
-  fit <- rstan::sampling(object = model)
-  expect_is(object = fit, class = "stanfit")
   
   data(Ig)
   fit <- DGU(usage.data = Ig,
-             mcmc.warmup = 500,
-             mcmc.steps = 1500,
+             mcmc.warmup = 250,
+             mcmc.steps = 500,
              mcmc.chains = 1,
              mcmc.cores = 1,
              hdi.level = 0.95,
-             adapt.delta = 0.95,
-             max.treedepth = 13)
+             adapt.delta = 0.90,
+             max.treedepth = 10)
   fit <- fit$glm
   expect_is(object = fit, class = "stanfit")
 })
